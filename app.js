06b231ab38df81a0e0fe20020c33b210d5b71313
Shai Yomtov <shaiy@liveperson.com>
2021-07-13 02:14:15 +0300

    set 418 path

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ app.get('/',(req,res) => {
 	res.send('Hello Shai!!!!!!');
 });
 
+app.get('/418path',(req,res) => {
+	res.status(418);
+	res.set();
+});
+
 app.listen(port,() => console.log(`Example app listening on port ${port}!`))
